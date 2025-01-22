88cf80ee686a5d8bae781ef3bf4809bb9172bbb1
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-01-22 23:21:32 +0100

    edit home component

--- components/madules/Home.js
+++ components/madules/Home.js
@@ -1,50 +1,35 @@
-import React from 'react'
+import React from "react";
 
-
-export default function Home() {
-
-
-
-
-
+export default function Home({img ,  title,   location,  roomCount,  meterage,    price}) {
   return (
+    <div className="card">
+      <img src={img} alt="House 6" className="card__img" />
+      <h5 className="card__title">{title} </h5>
+      <span className="card__like">
+        <i className="fa fa-heart"></i>
+      </span>
+      <div className="card__details">
+        <span className="">
+          <i className="fa fa-map-marker card__icon"></i>
+        </span>
+        <p className="card__text">Location : {location}</p>
+        <span className="">
+          <i className="fa fa-user card__icon"></i>
+        </span>
+        <p className="card__text">Rooms : {roomCount} </p>
+        <span className="">
+          <i className="fa fa-expand card__icon"></i>
+        </span>
+        <p className="card__text">Meterage :{meterage} </p>
+        <span className="">
+          <i className="fa fa-key card__icon"></i>
+        </span>
+        <p className="card__text">price: {price} $ </p>
+      </div>
 
-  
-  
-       <div className="card" >
-         <img src={home.img} alt="House 6" className="card__img" />
-         <h5 className="card__title">{home.title}  </h5>
-         <span className="card__like">
-           <i className="fa fa-heart"></i>
-         </span>
-         <div className="card__details">
-           <span className="">
-             <i className="fa fa-map-marker card__icon"></i>
-           </span>
-           <p className="card__text">Location : {home.location}</p>
-           <span className="">
-             <i className="fa fa-user card__icon"></i>
-           </span>
-           <p className="card__text">Rooms : {home.roomCount} </p>
-           <span className="">
-             <i className="fa fa-expand card__icon"></i>
-           </span>
-           <p className="card__text">Meterage :{home.meterage}  </p>
-           <span className="">
-             <i className="fa fa-key card__icon"></i>
-           </span>
-           <p className="card__text">price: {home.price} $ </p>
-         </div>
- 
-         <a href="#" className="btn btn-brown btn-card">
-          View
-         </a>
-       </div> 
-      
-  
-  
- 
- 
-    
-  )
+      <a href="#" className="btn btn-brown btn-card">
+        View
+      </a>
+    </div>
+  );
 }
