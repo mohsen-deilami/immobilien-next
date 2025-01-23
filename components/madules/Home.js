c976befb78799ff14f7c381636ad9d8751b1705d
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-01-23 14:59:40 +0100

    edit home details

--- components/madules/Home.js
+++ components/madules/Home.js
@@ -25,7 +25,7 @@ export default function Home({id ,img ,  title,   location,  roomCount,  meterag
         <span className="">
           <i className="fa fa-key card__icon"></i>
         </span>
-        <p className="card__text">price: {price} $ </p>
+        <p className="card__text">price: {price?.toLocaleString()} $ </p>
       </div>
 
       <Link href={`/homes/${id}`} className="btn btn-brown btn-card">
